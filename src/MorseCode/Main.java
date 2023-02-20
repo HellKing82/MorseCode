64fd55d7f34c46b12362f1d7efafa25a4fdad52f
linus <linushell82@gmail.com>
2023-02-20 09:35:17 +0100

    Ändrat i Main lite

--- src/MorseCode/Main.java
+++ src/MorseCode/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        System.out.println("Welcome to the Morse/English or English/Morse translate.");
-        System.out.println("If you want to translate Morse, use space between each letter and | when you want a space.");
+        System.out.println();
+        System.out.println("======== Welcome to the Morse/English or English/Morse translate ========");
+        System.out.println();
+        System.out.println("If you want to translate Morse, use space BETWEEN each letter/character and use | when you want a space");
         System.out.println();
         System.out.println("Input what you want to translate:");
         Scanner scan = new Scanner(System.in);
@@ -16,7 +17,7 @@ public class Main {
         text = text.toLowerCase();
         Logic InputLogic = new Logic();
         System.out.println();
-        System.out.println("Your translate input is: ");
+        System.out.println("Your translated input is: ");
         text = InputLogic.getMorseCode(text);
     }
 }
